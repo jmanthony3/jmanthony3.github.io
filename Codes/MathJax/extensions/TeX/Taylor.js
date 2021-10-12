d4308c19f9b8e745bb182b9046a44cb5ca6130c0
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2021-10-12 10:59:43 -0400

    Update Taylor.js

--- Codes/MathJax/extensions/TeX/Taylor.js
+++ Codes/MathJax/extensions/TeX/Taylor.js
@@ -1,7 +1,7 @@
 MathJax.Hub.Config({
 	TeX: {
 		Macros: {
-			expexpansion: ["Macro", "\\sum_{n=0}^{+\\infty} \\frac{x^n}{n!}"],
+			expexpansion: "\\sum_{n=0}^{+\\infty} \\frac{x^n}{n!}",
 			taylor: ["Macro","\\sum_{n=0}^{+\\infty} \\frac{{#1}^{(n)} \\left({#2}\\right)}{n!} {\\left( {#3} - {#2} \\right)}^n", 3],
 			taylorlog: ["Macro","\\sum_{n=1}^{+\\infty} {(-1)}^{n+1} \\frac{#1^n}{n}", 1],
 			taylorsin: ["Macro","\\sum_{n=0}^{+\\infty} \\frac{{(-1)}^n}{(2n+1)!} {#1}^{2n+1}", 1]
