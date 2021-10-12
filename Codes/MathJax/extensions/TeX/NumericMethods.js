1ce67d3a70e4d1360c27891df8670026d8864288
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2021-10-12 11:26:59 -0400

    Create NumericalMethods.js

---
+++ Codes/MathJax/extensions/TeX/NumericMethods.js
@@ -0,0 +1,9 @@
+MathJax.Hub.Config({
+	TeX: {
+		Macros: {
+			burgers: "{\\vec{b}}"
+		}
+	}
+});
+
+MathJax.Ajax.loadComplete("https://jmanthony3.github.io/Codes/MathJax/extensions/TeX/NumericalMethods.js");
