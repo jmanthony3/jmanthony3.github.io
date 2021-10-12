ae61c3c44ef045a1b738bebdb33fc3cc864e98b3
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2021-10-12 11:19:19 -0400

    Update Taylor.js

--- Codes/MathJax/extensions/TeX/Taylor.js
+++ Codes/MathJax/extensions/TeX/Taylor.js
@@ -2,11 +2,11 @@ MathJax.Hub.Config({
 	TeX: {
 		Macros: {
 			RR: "{\\mathbb{R}}",
-			bold: ["{\\mathbf #1}",1],
+			bold: ["{\\mathbf{#1}}",1],
 			expexpansion: "{\\sum_{n=0}^{+\\infty} \\frac{x^n}{n!}}",
-			taylor: ["{\\sum_{n=0}^{+\\infty} \\frac{{#1}^{(n)} \\left({#2}\\right)}{n!} {\\left( {#3} - {#2} \\right)}^n}", 3],
-			taylorlog: ["{\\sum_{n=1}^{+\\infty} {(-1)}^{n+1} \\frac{#1^n}{n}}", 1],
-			taylorsin: ["{\\sum_{n=0}^{+\\infty} \\frac{{(-1)}^n}{(2n+1)!} {#1}^{2n+1}}", 1]
+			taylor: ["{\\sum_{n=0}^{+\\infty} \\frac{{#1}^{(n)} \\left({#2}\\right)}{n!} {\\left( {#3} - {#2} \\right)}^{n}}", 3],
+			taylorlog: ["{\\sum_{n=1}^{+\\infty} {(-1)}^{n+1} \\frac{{#1}^{n}}{n}}", 1],
+			taylorsin: ["{\\sum_{n=0}^{+\\infty} \\frac{{(-1)}^{n}}{(2n+1)!} {#1}^{2n+1}}", 1]
 		}
 	}
 });
