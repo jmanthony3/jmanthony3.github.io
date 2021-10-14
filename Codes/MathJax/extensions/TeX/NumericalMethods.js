6ecd4e69d13de06d4e102f6cb940627ad8bc56c9
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2021-10-14 15:22:24 -0400

    added degree and angstrom.

--- Codes/MathJax/extensions/TeX/NumericalMethods.js
+++ Codes/MathJax/extensions/TeX/NumericalMethods.js
@@ -1,8 +1,10 @@
 MathJax.Hub.Config({
 	TeX: {
 		Macros: {
+			angstrom: "{\\text{\\AA}}",
 			burgers: "{\\vec{b}}",
-			converge: ["{\\mathcal\{O\}({#1})}", 1]
+			converge: ["{\\mathcal{O}( {#1} )}", 1],
+			degree: "{^{\circ}}",
 		}
 	}
 });
