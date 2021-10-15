a8a46859f692752a7a60d62d3df9ba9bd952cf8e
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2021-10-15 09:56:24 -0400

    foo

---
+++ Codes/MathJax/extensions/TeX/MechanicalMetallurgy.js
@@ -0,0 +1,14 @@
+MathJax.Hub.Config({
+	TeX: {
+		Macros: {
+			angstrom: "{\\mathring{A}}",
+			burgers: "{\\vec{b}}",
+			degree: "{^{\\circ}}",
+			degreeC: "{^{\\circ}C}",
+			degreeF: "{^{\\circ}F}",
+			// converge: ["{\\mathcal{O}({#1})}", 1],
+		}
+	}
+});
+
+MathJax.Ajax.loadComplete("https://jmanthony3.github.io/Codes/MathJax/extensions/TeX/MechanicalMetallurgy.js");
