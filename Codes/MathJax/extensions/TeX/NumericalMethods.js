8c4d0ba6846edd82825295c1472a630848b8b127
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2021-10-15 09:56:24 -0400

    foo

--- Codes/MathJax/extensions/TeX/NumericalMethods.js
+++ Codes/MathJax/extensions/TeX/NumericalMethods.js
@@ -1,12 +1,14 @@
 MathJax.Hub.Config({
 	TeX: {
 		Macros: {
-			angstrom: "{\\mathring{A}}",
-			burgers: "{\\vec{b}}",
+			converge: ["{\\mathcal{O}({#1})}", 1],
 			degree: "{^{\\circ}}",
 			degreeC: "{^{\\circ}C}",
 			degreeF: "{^{\\circ}F}",
-			converge: ["{\\mathcal{O}({#1})}", 1],
+			expexpansion: "{\\sum_{n=0}^{+\\infty} \\frac{x^{n}}{n!}}",
+			taylor: ["{\\sum_{n=0}^{+\\infty} \\frac{{#1}^{(n)} \\left({#2}\\right)}{n!} {\\left( {#3} - {#2} \\right)}^{n}}", 3],
+			taylorlog: ["{\\sum_{n=1}^{+\\infty} {(-1)}^{n+1} \\frac{{#1}^{n}}{n}}", 1],
+			taylorsin: ["{\\sum_{n=0}^{+\\infty} \\frac{{(-1)}^{n}}{(2n+1)!} {#1}^{2n+1}}", 1],
 		}
 	}
 });
