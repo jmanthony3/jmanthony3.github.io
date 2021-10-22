d7a0bdc2becd3458b7b5d4445f8babdf5baa54cd
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2021-10-22 16:57:05 -0400

    revert to old instance definition.

--- Codes/MathJax/extensions/TeX/NumericalMethods.js
+++ Codes/MathJax/extensions/TeX/NumericalMethods.js
@@ -1,6 +1,9 @@
-MathJax.Hub.Register.StartupHook("TeX Jax Ready", function () {
-	MathJax.InputJax.TeX.Definitions.Add({
-		macros: {
+MathJax.Hub.Config({
+	TeX: {
+		Macros: {
+// MathJax.Hub.Register.StartupHook("TeX Jax Ready", function () {
+// 	MathJax.InputJax.TeX.Definitions.Add({
+// 		macros: {
 			converge: ["{\\mathcal{O}({#1})}", 1],
 			degree: "{^{\\circ}}",
 			degreeC: "{^{\\circ}C}",
@@ -11,8 +14,11 @@ MathJax.Hub.Register.StartupHook("TeX Jax Ready", function () {
 			taylor: ["{\\sum_{n=0}^{+\\infty} \\frac{{#1}^{(n)} \\left({#2}\\right)}{n!} {\\left( {#3} - {#2} \\right)}^{n}}", 3],
 			taylorlog: ["{\\sum_{n=1}^{+\\infty} {(-1)}^{n+1} \\frac{{#1}^{n}}{n}}", 1],
 			taylorsin: ["{\\sum_{n=0}^{+\\infty} \\frac{{(-1)}^{n}}{(2n+1)!} {#1}^{2n+1}}", 1],
+// 		}
+// 	});
+// });
 		}
-	});
+	}
 });
 
 MathJax.Ajax.loadComplete("https://jmanthony3.github.io/Codes/MathJax/extensions/TeX/NumericalMethods.js");
