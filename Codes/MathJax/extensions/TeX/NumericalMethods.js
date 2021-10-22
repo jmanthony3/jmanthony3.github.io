65863c481d369a6aee232cf0e5dcc0c0129af861
Joby Anthony III <62029937+jmanthony3@users.noreply.github.com>
2021-10-22 16:59:49 -0400

    berid comments.

--- Codes/MathJax/extensions/TeX/NumericalMethods.js
+++ Codes/MathJax/extensions/TeX/NumericalMethods.js
@@ -1,9 +1,6 @@
 MathJax.Hub.Config({
 	TeX: {
 		Macros: {
-// MathJax.Hub.Register.StartupHook("TeX Jax Ready", function () {
-// 	MathJax.InputJax.TeX.Definitions.Add({
-// 		macros: {
 			converge: ["{\\mathcal{O}({#1})}", 1],
 			degree: "{^{\\circ}}",
 			degreeC: "{^{\\circ}C}",
@@ -14,9 +11,6 @@ MathJax.Hub.Config({
 			taylor: ["{\\sum_{n=0}^{+\\infty} \\frac{{#1}^{(n)} \\left({#2}\\right)}{n!} {\\left( {#3} - {#2} \\right)}^{n}}", 3],
 			taylorlog: ["{\\sum_{n=1}^{+\\infty} {(-1)}^{n+1} \\frac{{#1}^{n}}{n}}", 1],
 			taylorsin: ["{\\sum_{n=0}^{+\\infty} \\frac{{(-1)}^{n}}{(2n+1)!} {#1}^{2n+1}}", 1],
-// 		}
-// 	});
-// });
 		}
 	}
 });
